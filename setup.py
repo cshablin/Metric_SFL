ebe59bf4738238f1327db9cfd6b606abf57d1447
Amir Elmishali <amir9979@gmail.com>
2020-07-19 17:12:59 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name='sfl_diagnoser',
     version='0.0.1',
     packages=find_packages(),
-    url='https://github.com/amir9979/sfl-diagnoser',
     license='',
     author='Amir Elmishali',
     author_email='amir9979@gmail.com',
