0e1bf9586008dbe20c68ffe17d4e15b0332d6e75
cshablin <Carmel.Shablin@kla-tencor.com>
2021-04-30 21:08:28 +0300

    test commit in branch

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sfl.Diagnoser.diagnoserUtils import write_json_planning_file, read_json_pla
 from sfl.Diagnoser.Experiment_Data import Experiment_Data
 from sfl.Diagnoser.Diagnosis_Results import Diagnosis_Results
 
-ei = read_json_planning_file(r'5a509030a946646780e963b9f492a4eaff734116')
+ei = read_json_planning_file(r'3131_56cd921f')
+# ei = read_json_planning_file(r'506bd018b3ca638cd0c9d1bdad627f6468a05bee')
 ei.diagnose()
 print(Diagnosis_Results(ei.diagnoses, ei.initial_tests, ei.error, ei.pool, ei.get_id_bugs()).metrics)
